b643916066244d76dba4849d31428caea6966c7b
Nhan <nhan@iMac-Pro.local>
2023-02-22 22:24:51 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './App.css';
 import HomePage from './components/HomePage/HomePage';
 import { useEffect, useState } from 'react';
 import myMusic from './audio/audio1.mp3'
-import useSound from 'use-sound'
+
 
 function App() {
   const [menuToggle, setMenuToggle] = useState(false)
   const [stickyClass, setStickyClass] = useState('')
+  const [isPlay, setIsPlay] = useState(false)
   const [audio] = useState(new Audio(myMusic))
   // Hiệu ứng navbar
-  const stickNav = () => { 
+  const stickNav = () => {
     let windowHeight = window.scrollY;
     (windowHeight > 64) ? setStickyClass('bg-show') : setStickyClass('')
   }
@@ -29,10 +30,59 @@ function App() {
     <li><a className='p-3 h-10 block hover:bg-[hsla(0,0%,100%,.1)]' href="">Account</a></li>
     <li><a className='p-3 h-10 block hover:bg-[hsla(0,0%,100%,.1)]' href="">Log Out</a></li>
   </ul> : ""
-    // Toggle menu navbar thò thụt
+  // Toggle menu navbar thò thụt
+
+  // Handle Play Audio
   const handlePlay = () => {
-    audio.play()
+    setIsPlay(!isPlay)
   }
+
+  useEffect(() => {
+    (isPlay) ? audio.play() : audio.pause()
+  }, [isPlay])
+
+  const elementIconPlayingFooter = (isPlay) ? <svg
+    role="img"
+    height="16"
+    width="16"
+    aria-hidden="true"
+    viewBox="0 0 16 16"
+    data-encore-id="icon"
+    className="Svg-sc-ytk21e-0 uPxdw">
+    <path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path>
+  </svg> : <svg
+    role="img"
+    height="16"
+    width="16"
+    aria-hidden="true"
+    viewBox="0 0 16 16"
+    data-encore-id="icon"
+    className="Svg-sc-ytk21e-0 uPxdw">
+    <path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path>
+  </svg>
+
+  const elementIconPlayingItem = (isPlay) ? <svg
+    role="img"
+    height="24"
+    width="24"
+    aria-hidden="true"
+    viewBox="0 0 24 24"
+    data-encore-id="icon"
+    className="Svg-sc-ytk21e-0 uPxdw">
+    <path d="M5.7 3a.7.7 0 0 0-.7.7v16.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V3.7a.7.7 0 0 0-.7-.7H5.7zm10 0a.7.7 0 0 0-.7.7v16.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V3.7a.7.7 0 0 0-.7-.7h-2.6z"></path>
+  </svg> : <svg
+    role="img"
+    height="24"
+    width="24"
+    aria-hidden="true"
+    viewBox="0 0 24 24"
+    data-encore-id="icon"
+    className="Svg-sc-ytk21e-0 uPxdw">
+    <path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path>
+  </svg>
+  // Handle Play Audio
+
+
 
   return (
     <div className="App">
@@ -185,7 +235,7 @@ function App() {
                       <div className='album-img mb-4 relative'>
                         <img className='rounded drop-shadow-[0_8px_24px_rgba(0,0,0,0.5)]' src="https://i.scdn.co/image/ab67706f00000002ca5a7517156021292e5663a6" alt="" />
                         <button onClick={handlePlay} className='group-hover:opacity-100 group-hover:translate-y-0 group-hover:shadow-xl w-12 h-12 cursor-default rounded-[50%] bg-[#1ed760] flex items-center justify-center absolute bottom-2 right-2 hover:scale-105 transition-all duration-300 opacity-0 translate-y-2'>
-                          <svg
+                          {/* <svg
                             role="img"
                             height="24"
                             width="24"
@@ -194,7 +244,8 @@ function App() {
                             data-encore-id="icon"
                             className="Svg-sc-ytk21e-0 uPxdw">
                             <path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path>
-                          </svg>
+                          </svg> */}
+                          {elementIconPlayingItem}
                         </button>
                       </div>
                       <div className='album-content text-[#fff]'>
@@ -597,7 +648,17 @@ function App() {
                     </svg>
                   </button>
                   <button className='w-8 h-8 flex justify-center items-center bg-[#fff] rounded-[50%] hover:scale-105'>
-                    <svg
+                    {/* <svg
+                      role="img"
+                      height="16"
+                      width="16"
+                      aria-hidden="true"
+                      viewBox="0 0 16 16"
+                      data-encore-id="icon"
+                      class="Svg-sc-ytk21e-0 uPxdw">
+                      <path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path>
+                    </svg> */}
+                    {/* <svg
                       role="img"
                       height="16"
                       width="16"
@@ -606,7 +667,8 @@ function App() {
                       data-encore-id="icon"
                       className="Svg-sc-ytk21e-0 uPxdw">
                       <path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path>
-                    </svg>
+                    </svg> */}
+                    {elementIconPlayingFooter}
                   </button>
                   <button className='w-8 h-8 flex justify-center items-center fill-[#fff] opacity-60 hover:opacity-100'>
                     <svg
